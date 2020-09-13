9affd58260ad56254f89ffb72ad4ebc2e7a47a79
royrx93 <royfmttm@gmail.com>
2020-09-13 22:58:04 +0800

    server get request comment

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const http = require('http')
 const querystring = require('querystring')
 
 const server = http.createServer((req,res) =>{
-    console.log(req.method)
-    const url = req.url
+    console.log(req.method) // GET
+    const url = req.url     // 获取请求的完整url
     console.log('url:', url)
-    req.query = querystring.parse(url.split('?')[1])
-    console.log('query: ',req.query)
-    res.end(
+    req.query = querystring.parse(url.split('?')[1]) //解析 querystring
+    console.log('query: ',req.query)    
+    res.end(                    // 将querystring 返回
         JSON.stringify(req.query)
         )
 })
 
 server.listen(8000)
+console.log('okk')
